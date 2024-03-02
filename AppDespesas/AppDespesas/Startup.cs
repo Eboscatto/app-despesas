11069d96aa8c700a43a6588f19dd23d4d73a1303
Eboscaatto <e_boscatto@hotmail.com>
2024-03-02 13:55:07 -0300

    Configuração do script de acesso para o MySql e criado o banco de dados com a tabela Despesa

--- AppDespesas/AppDespesas/Startup.cs
+++ AppDespesas/AppDespesas/Startup.cs
@@ -37,7 +37,8 @@ namespace AppDespesas
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<AppDespesasContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("AppDespesasContext")));
+                   options.UseMySql(Configuration.GetConnectionString("AppDespesasContext"), builder =>
+                        builder.MigrationsAssembly("AppDespesas")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
